4e4c7414a694e482daf89dbece25e904824c2ec0
sajjad ramezani <sajjadramezani780@gmail.com>
2023-08-30 21:21:25 +0430

    first commit

--- src/services/articles.js
+++ src/services/articles.js
@@ -1,16 +1,24 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-  const rapidKey = import.meta.env.VITE_RAPIDAPI_ARTICLES_KEY;
+
+
 export const articleApi = createApi({
-    reducerPath:'articleApi',
-    baseQuery: fetchBaseQuery({ baseUrl:'https://article-extractor-and-summarizer.p.rapidapi.com/'}),
-    prepareHeaders:(headers)=>{
-     headers.set('X-RapidAPI-Key',rapidKey),
-     headers.set('X-RapidAPI-Host','article-extractor-and-summarizer.p.rapidapi.com')
-     return headers;
-    },
-    endpoints: (builder) => ({
-      getsummeri: builder.query({
-        query: (params) => 'test',
-      }),
+    reducerPath: 'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', "de35d22adfmsh06f52c24e3b3441p15d2afjsn72fcfb4e3f4b");
+            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+
+            return headers;
+        },
     }),
-});
+    endpoints: (builder) => ({
+        getSummary: builder.query({
+            // encodeURIComponent() function encodes special characters that may be present in the parameter values
+            // If we do not properly encode these characters, they can be misinterpreted by the server and cause errors or unexpected behavior. Thus that RTK bug
+            query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+        }),
+    }),
+})
+
+export const { useLazyGetSummaryQuery } = articleApi
--- src/services/store.js
+++ src/services/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import { articleApi } from "./articles";
 
-
 export const store = configureStore({
-    reducer:{
-        [articleApi.reducerPath]:articleApi.reducer
+    reducer: {
+        [articleApi.reducerPath]: articleApi.reducer,
     },
-    middleware:(getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
 })
